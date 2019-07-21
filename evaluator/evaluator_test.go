8a2a1a197fe766fe9e4504ed8c171a5c41cea687
Jordan Rhodes <jsrhodes15@gmail.com>
2019-07-21 18:37:02 -0400

    Start evaluator - eval Integer literal

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,45 @@
+package evaluator
+
+import (
+	"github.com/jsrhodes15/monkey/lexer"
+	"github.com/jsrhodes15/monkey/object"
+	"github.com/jsrhodes15/monkey/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{input: "5", expected: 5},
+		{input: "10", expected: 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got =%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
