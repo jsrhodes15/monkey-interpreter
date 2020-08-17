3a1a76aca0187b399fb1246660d505db6cd55bea
Jordan Rhodes <jsrhodes15@gmail.com>
2020-08-17 08:51:42 -0400

    Add 'environment' map, refactor Eval, at support for 'let' statements

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/jsrhodes15/monkey-interpreter/object"
+
 	"github.com/jsrhodes15/monkey-interpreter/evaluator"
 	"github.com/jsrhodes15/monkey-interpreter/lexer"
 	"github.com/jsrhodes15/monkey-interpreter/parser"
@@ -14,6 +16,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -32,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
