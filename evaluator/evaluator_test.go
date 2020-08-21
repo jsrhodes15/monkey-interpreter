f0d2381e8e737da673b7d01698c180387583cf2c
Jordan Rhodes <jsrhodes15@gmail.com>
2020-08-21 08:46:31 -0400

    Add test for closure behavior

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -133,6 +133,25 @@ if (10 >1) {
 `,
 			10,
 		},
+		{
+			`
+let f = fn(x) {
+  return x;
+  x + 10;
+};
+f(10);`,
+			10,
+		},
+		{
+			`
+let f = fn(x) {
+   let result = x + 10;
+   return result;
+   return 10;
+};
+f(10);`,
+			20,
+		},
 	}
 
 	for _, tt := range tests {
@@ -262,6 +281,18 @@ func TestFunctionApplication(t *testing.T) {
 	for _, tt := range tests {
 		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
+}
+
+func TestClosures(t *testing.T) {
+	input := `
+	let newAdder = fn(x) {
+		fn(y) { x + y };
+	};
+	
+	let addTwo = newAdder(2);
+	addTwo(2);`
+
+	testIntegerObject(t, testEval(input), 4)
 }
 
 func testEval(input string) object.Object {
