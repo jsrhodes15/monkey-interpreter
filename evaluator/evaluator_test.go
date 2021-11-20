dbfc24a4a4f91bd1ee83476d8637f81e14a2e368
Jordan Rhodes <jsrhodes15@gmail.com>
2021-11-20 14:23:49 -0500

    Add array indexing and builtins

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -335,8 +335,16 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len("")`, 0},
 		{`len("four")`, 4},
 		{`len("hello world")`, 11},
+		{`len([])`, 0},
+		{`len([1, 2, 3])`, 3},
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`first([3, 2, 1])`, 3},
+		{`first("hello")`, "argument to `first` must be ARRAY, got STRING"},
+		{`first([42], ["foo"])`, "wrong number of arguments. got=2, want=1"},
+		{`last([3, 2, 1])`, 1},
+		{`last("hello")`, "argument to `last` must be ARRAY, got STRING"},
+		{`last([42], ["foo"])`, "wrong number of arguments. got=2, want=1"},
 	}
 
 	for _, tt := range tests {
@@ -376,6 +384,62 @@ func TestArrayLiterals(t *testing.T) {
 	testIntegerObject(t, result.Elements[0], 1)
 	testIntegerObject(t, result.Elements[1], 4)
 	testIntegerObject(t, result.Elements[2], 6)
+}
+
+func TestArrayIndexExpressions(t *testing.T) { tests := []struct {
+	input string
+	expected interface{}
+}{
+	{
+		"[1, 2, 3][0]",
+		1,
+	},
+	{
+		"[1, 2, 3][1]",
+		2,
+	},
+	{
+		"[1, 2, 3][2]",
+		3,
+	},
+	{
+		"let i = 0; [1][i];",
+		1,
+	},
+	{
+		"[1, 2, 3][1 + 1]",
+		3,
+	},
+	{
+		"let myArray = [1, 2, 3]; myArray[2];",
+		3,
+	},
+	{
+		"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];",
+		6,
+	},
+	{
+		"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]",
+		2,
+	},
+	{
+		"[1, 2, 3][3]",
+		nil,
+	},
+	{
+		"[1, 2, 3][-1]",
+		nil,
+	},
+}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
 }
 
 func testEval(input string) object.Object {
